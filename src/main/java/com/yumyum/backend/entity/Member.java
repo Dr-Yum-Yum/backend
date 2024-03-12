104b202b9410f5c0c724d90017a3f37ee4a06d6f
BrokenFinger98 <dbtjsdn980818@gmail.com>
2024-03-12 21:01:06 +0900

    Member class 생성

--- src/main/java/com/yumyum/backend/entity/Member.java
+++ src/main/java/com/yumyum/backend/entity/Member.java
@@ -1,4 +1,41 @@
 package com.yumyum.backend.entity;
 
-public class Member {
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Getter
+public class Member extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "member_id")
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String loginId;
+
+    @Column(nullable = false, unique = false)
+    private String password;
+
+    @Column(nullable = false, unique = false)
+    private String name;
+
+    @Column(nullable = false, unique = false)
+    private String phone;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = false)
+    private String nickname;
+
+    @Column(nullable = false, unique = true)
+    private String profileImagePath;
+
+    @Column(nullable = false, unique = false)
+    private String backgroundImagePath;
 }
